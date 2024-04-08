e63546ff0271da04b3740100f2c6b4f5e7072294
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-08 22:19:31 +0300

    make build

---
+++ index.0e1d4c38.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=t.getBoundingClientRect(),l=(c.width-20-n.height)/2;t.style.top="".concat(l,"px"),t.style.left="".concat(l,"px");
+//# sourceMappingURL=index.0e1d4c38.js.map
